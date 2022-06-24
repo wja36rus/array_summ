4c8fd81523a89e54d2a8776b7bc50fa7d5b6e498
wja36rus <48429607+wja36rus@users.noreply.github.com>
2022-06-25 00:17:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const arr2 = [[1, 2], [4, 5]];
 const arr3 = [[4.25, 5.15, 8.87], [2.15, 1.39, 3.67], [1.05, 2.81, 1.21]];
 const arr4 = [[1, 2, 3, 7], [1, 2, 3, 7], [1, 2, 3, 7]];
 const arr5 = [[0.1, 0.2, 0.1, 0.2], [1, 2, 3, 7], [0.001, 0.002, 0.001, 0.002]];
+const arr6 = [[1], [4], [6]];
 
 function getSumm(arr) {
     const stop = arr.length;
@@ -39,3 +40,4 @@ console.log(getSumm(arr2));
 console.log(getSumm(arr3));
 console.log(getSumm(arr4));
 console.log(getSumm(arr5));
+console.log(getSumm(arr6));
