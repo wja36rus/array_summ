9ea0c2e9278cb448f0a6ee24d5f8094b39e37169
wja36rus <48429607+wja36rus@users.noreply.github.com>
2022-06-24 23:57:37 +0300

    ready

---
+++ index.js
@@ -0,0 +1,55 @@
+const arr1 = [
+    [1, 2, 3]
+];
+
+const arr2 = [
+    [1, 2],
+    [4, 5]
+];
+
+const arr3 = [
+    [4.25, 5.15, 8.87],
+    [2.15, 1.39, 3.67],
+    [1.05, 2.81, 1.21],
+];
+
+const arr4 = [
+    [1, 2, 3, 7],
+    [1, 2, 3, 7],
+    [1, 2, 3, 7],
+];
+
+function getSumm(arr) {
+    const stop = arr.length;
+    const stop_child = arr[0].length;
+
+
+    if (stop > 1) {
+        const preResult = [];
+
+        for (let j = 0; j < stop_child; j++) {
+            preResult.push([])
+        }
+
+        for (let i = 0; i < stop; i++) {
+            for (let j = 0; j < stop_child; j++) {
+                preResult[j].push(arr[i][j])
+            }
+        }
+
+        const result = [];
+
+        for (let i = 0; i < preResult.length; i++) {
+            result.push(preResult[i].reduce((partialSum, a) => partialSum + a, 0).toString().replace('.', ','));
+        }
+
+        return result;
+    } else {
+        return arr[0];
+    }
+}
+
+console.log(getSumm(arr1));
+console.log(getSumm(arr2));
+console.log(getSumm(arr3));
+console.log(getSumm(arr4));
