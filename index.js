a72db6c3a355d1814ceda9b30e998a3cc7f28f7d
wja36rus <48429607+wja36rus@users.noreply.github.com>
2022-06-29 00:16:41 +0300

    result by all day

--- index.js
+++ index.js
@@ -1,127 +1,49 @@
 const data = [
-    {
-        all_day: 6,
-        mass: [
-            0.5655,
-            14.151,
-            1.135
-        ],
-        period: [
-            {
-                slide: [
-                    {value: 3},
-                    {value: 2},
-                    {value: 5}
-                ]
-            },
-            {
-                slide: [
-                    {value: 1},
-                    {value: 2}
-                ]
-            },
-            {
-                slide: [
-                    {value: 4},
-                    {value: 6},
-                    {value: 2}
-                ]
-            }
-        ]
-    }
+    {day_start: 1, day_stop: 2, all_day: 1},
+    {day_start: 3, day_stop: 5, all_day: 2},
+    {day_start: 6, day_stop: 9, all_day: 3}
 ];
 
-/**
- *
- * @param arr
- * @returns {[]}
- */
-function getPeriodAndSlide(arr) {
-    const stop = arr.length;
-    const period = [];
+function getByAllDay(datas) {
+
+    const stop = datas.length;
+
+    const data = [];
+    for (let i = 0; i < stop; i++) {
+        data.push({
+            day_start: datas[i].day_start,
+            day_stop: 0,
+            all_day: datas[i].all_day,
+        })
+    }
+
+    const all = [];
+    for (let i = 0; i < stop; i++) {
+        all.push(data[i].all_day);
+    }
+    const result_all = all.reduce((acc, obj) => acc + obj, 0);
+
     const result = [];
-
+    const arr = data.reverse();
     for (let i = 0; i < stop; i++) {
-        period.push([]);
-        for (let is = 0; is < arr[i].period.length; is++) {
-            period[i].push([]);
-            for (let iss = 0; iss < arr[i].period[is].slide.length; iss++) {
-                period[i][is].push(arr[i].period[is].slide[iss].value * arr[i].all_day);
-            }
+        if (i === 0) {
+            result.push({
+                day_start: result_all - arr[i].all_day + 1,
+                day_stop: result_all,
+                all_day: arr[i].all_day
+            })
+        } else {
+            const is = i - 1;
+            result.push({
+                day_start: result[is].day_start - arr[i].all_day,
+                day_stop: result[is].day_start - 1,
+                all_day: arr[i].all_day
+            })
         }
     }
 
-    for (let i = 0; i < period.length; i++) {
-        result.push([]);
-        for (let is = 0; is < period[i].length; is++) {
-            result[i].push(period[i][is].reduce((partialSum, a) => partialSum + a, 0));
-        }
-    }
+    console.log(result.reverse());
 
-    return result;
 }
 
-/**
- *
- * @param arr
- * @returns {[]}
- */
-function getMass(arr) {
-    const slide = getPeriodAndSlide(arr);
-    const stop = slide.length;
-
-    const c = [];
-    for (let i = 0; i < stop; i++) {
-        for (let is = 0; is < slide[i].length; is++) {
-            c.push({mass: arr[i].mass, value: slide[i][is]});
-        }
-    }
-
-    const cc = [];
-    for (let i = 0; i < c.length; i++) {
-        cc.push([]);
-        for (let is = 0; is < c[i].mass.length; is++) {
-            cc[i].push(c[i].mass[is] * c[i].value);
-        }
-    }
-
-    return cc;
-}
-
-
-/**
- *
- * @param arr
- * @returns {[]|*}
- */
-function getSumm(arr) {
-    const stop = arr.length;
-    const stop_child = arr[0].length;
-
-
-    if (stop > 1) {
-        const preResult = [];
-
-        for (let j = 0; j < stop_child; j++) {
-            preResult.push([])
-        }
-
-        for (let i = 0; i < stop; i++) {
-            for (let j = 0; j < stop_child; j++) {
-                preResult[j].push(arr[i][j])
-            }
-        }
-
-        const result = [];
-
-        for (let i = 0; i < preResult.length; i++) {
-            result.push(preResult[i].reduce((partialSum, a) => partialSum + a, 0).toString().replace('.', ','));
-        }
-
-        return result;
-    } else {
-        return arr[0];
-    }
-}
-
-getSumm(getMass(data));
+getByAllDay(data);
