ee41d914268842b836aeabd01909dc605c506ac7
wja36rus <48429607+wja36rus@users.noreply.github.com>
2022-06-29 22:42:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const data = [
     {day_start: 1, day_stop: 2, all_day: 2},
-    {day_start: 4, day_stop: 5, all_day: 3},
-    {day_start: 6, day_stop: 10, all_day: 4},
-    {day_start: 11, day_stop: 13, all_day: 3}
-
+    {day_start: 3, day_stop: 5, all_day: 3},
+    {day_start: 6, day_stop: 10, all_day: 5},
+    {day_start: 9, day_stop: 13, all_day: 3}
 ];
 
 
@@ -16,30 +15,26 @@ const getData = (data, index) => {
             const is = i + 1;
             result.push({
                 day_start: data[i].day_start,
-                data_stop: data[is].day_start - 1,
-                all_day: data[i].day_stop - data[i].day_start + 1
+                day_stop: data[is].day_start - 1,
+                all_day: data[is].day_start - data[i].day_start
             });
         } else if (i === index) {
             result.push({
                 day_start: data[i].day_start,
-                data_stop: data[i].day_start + data[i].all_day - 1,
+                day_stop: data[i].day_start + data[i].all_day - 1,
                 all_day: data[i].all_day,
             });
         } else if (i > index) {
-            const is = i - 1;   
-            console.log(result[is])     
+            const is = i - 1;
             result.push({
-                day_start: result[is].day_stop,
-                data_stop: data[is].day_stop + data[i].all_day - 1,
-                all_day: data[i].day_stop - data[is].day_stop + 1,
+                day_start: result[is].day_stop + 1,
+                day_stop: result[is].day_stop + data[i].all_day - 1,
+                all_day: data[i].day_stop - result[is].day_stop + 1,
             });
         }
     }
-    
-    
-    for (let i = 0; i < result.length; i++) {
-        //console.log(result[i]);
-    }
+
+    return result
 };
 
-getData(data, 1);
+getData(data, 3);
