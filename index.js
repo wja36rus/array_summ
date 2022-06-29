b1beaa013ef990538887874baa8ec9757df96a82
wja36rus <48429607+wja36rus@users.noreply.github.com>
2022-06-29 21:41:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,23 +10,36 @@ const data = [
 const getData = (data, index) => {
     const stop = data.length;
 
+    const result = [];
     for (let i = 0; i < stop; i++) {
         if (i < index) {
             const is = i + 1;
-            data[i].data_stop = data[is].day_start - 1;
-            data[i].all_day = data[i].day_stop + data[i].day_start - 1;
+            result.push({
+                day_start: data[i].day_start,
+                data_stop: data[is].day_start - 1,
+                all_day: data[i].day_stop - data[i].day_start + 1
+            });
         } else if (i === index) {
-            console.log("current");
-        } else {
-            const is = i - 1;
-            data[i].data_start = data[is].day_stop;
-            data[i].data_stop = data[is].day_start + data[is].all_day - 1;
-            data[i].all_day = data[i].day_stop + data[i].day_start - 1;
+            result.push({
+                day_start: data[i].day_start,
+                data_stop: data[i].day_start + data[i].all_day - 1,
+                all_day: data[i].all_day,
+            });
+        } else if (i > index) {
+            const is = i - 1;   
+            console.log(result[is])     
+            result.push({
+                day_start: result[is].day_stop,
+                data_stop: data[is].day_stop + data[i].all_day - 1,
+                all_day: data[i].day_stop - data[is].day_stop + 1,
+            });
         }
     }
     
     
-    return data;
+    for (let i = 0; i < result.length; i++) {
+        //console.log(result[i]);
+    }
 };
 
-console.log(getData(data, 1));
+getData(data, 1);
